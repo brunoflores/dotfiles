583565a5c3ca3430c3162a00719ea9b5d7753c7c
Bruno Flores <bruno@brunoflores.com.br>
2023-09-17 16:52:58 +1000

    Update script so that we can easily reinstall packages.

--- sys_update.sh
+++ sys_update.sh
@@ -10,7 +10,11 @@ git pull origin master
 sudo pacman -Syu
 fish_update_completions
 
-pacman -Qe > ./arch/pacman_Qe.txt
+pacman -Qqen > ./arch/pacman_Qe.txt
+# We can use the output to reinstall the same list
+# of packages with
+# `pacman -S - < ./arch/pacman_Qe.txt`
+
 git add ./arch/pacman_Qe.txt
 git commit -m "Update arch system."
 git push origin master
