d0daaf10d5e5ed8ec9ceb9f6d95fce7239de329c
Bruno Flores <bruno@brunoflores.com.br>
2024-07-07 15:47:49 +1000

    Supress welcome message in fish.

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Supress welcome message
+set fish_greeting
+
 # Copy and Paste.
 # Requires `pacman -S xsel`
 alias pbcopy "xsel --clipboard --input"
